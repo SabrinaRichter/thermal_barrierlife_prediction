b09ff06661fb74c7b9e3851c46324804c351e5a5
Julius Polz <julius.polz@kit.edu>
2021-09-11 09:45:57 +0200

    ensemble prediction to weighted  average

--- thermal_barrierlife_prediction/ensemble_estimator.py
+++ thermal_barrierlife_prediction/ensemble_estimator.py
@@ -1,7 +1,8 @@
 import numpy as np
-
+from thermal_barrierlife_prediction.load_data import read_data
 from thermal_barrierlife_prediction import EstimatorCNN
-from thermal_barrierlife_prediction.evaluation import performance_report, performance_report_magnification
+from thermal_barrierlife_prediction.evaluation import performance_report, performance_report_magnification            
+import os
 
 
 class EnsembleEstimator:
@@ -33,11 +34,37 @@ class EnsembleEstimator:
 
     def evaluate_models(
             self,
+            runs = 15,
     ):
         for model_type in self.estimators.keys():
             for estim in self.estimators[model_type]:
-                y_pred = estim.predict(val_idx=estim.val_idx)  # Predicts with saved val data
+                y_pred = []
+                for i in range(runs):
+                    y_pred.append(estim.predict(val_idx=estim.val_idx))  # Predicts with saved val data
+                y_pred = np.mean(np.array(y_pred), axis=0)
                 y_true = estim.data['lifetime'][estim.val_idx]
                 y_max = estim.data['magnification'][estim.val_idx]
                 estim.performance_report = performance_report(y_true, y_pred)
                 estim.performance_report_magnification = performance_report_magnification(y_true, y_pred, y_max)
+
+
+
+    def predict_val(
+        self,
+        runs = 15,
+    ):
+        y_model_avg = []
+        model_scores = []
+        for model_type in self.estimators.keys():
+            for estim in self.estimators[model_type]:
+                val_data = read_data(csv_file_path=False, tiff_folder_path='../data/valid/')['greyscale']
+                y_pred = []
+                for i in range(runs):
+                    y_pred.append(estim.predict(val_data=val_data))  # Predicts with saved val data
+                y_model_avg.append(np.mean(np.array(y_pred), axis=0))
+                model_scores.append(1/estim.performance_report['mean_absolute_error'])
+        model_weights = np.array(model_scores)/np.sum(model_scores)
+        y_pred = y_model_avg*model_weights
+        print(y_pred.shape)
+        y_pred = np.mean(y_pred, axis=0)
+        return y_pred
